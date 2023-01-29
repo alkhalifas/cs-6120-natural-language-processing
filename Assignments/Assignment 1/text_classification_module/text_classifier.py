7194602fdfa56a88f6b421be71df444b83bd7c85
Saleh Alkhalifa <salehesam@gmail.com>
2023-01-29 17:46:17 -0500

    assignment 1 done

--- Assignments/Assignment 1/text_classification_module/text_classifier.py
+++ Assignments/Assignment 1/text_classification_module/text_classifier.py
@@ -1,27 +1,11 @@
-import numpy as np
-import math
-import pandas as pd
 import re
 import string
-import random
 import json
-
 import nltk
-from nltk.corpus import stopwords
-from nltk.stem import PorterStemmer
-from nltk.tokenize import word_tokenize
-
-from sklearn.metrics import confusion_matrix
-
-
-from tqdm import tqdm
 import warnings
-import seaborn as sns
-import matplotlib.pyplot as plt
 
 warnings.filterwarnings("ignore")
 nltk.download('popular', quiet=True)
-
 stopword = nltk.corpus.stopwords.words('english')
 ps = nltk.PorterStemmer()
 
@@ -107,8 +91,10 @@ def text_classifier():
                                                    
 ####################################################
                                                    """)
-    print("Welcome to the movie review classifier!")
+    print("Welcome to the movie review classifier!\n")
+    print("Instructions:")
     print("Enter your review and we will detect the sentiment.")
+    print("Example: 'This is a great movie', or 'this is a terrible movie'\n")
     print("To exit the application, enter 'X' \n")
 
     while True:
